5d6d59b7e2d5cde7ad71b7a58a6c2dbcc85ba4a9
James Bray <jrbrayjr@gmail.com>
2025-05-05 08:11:54 -0500

    remove console log on http request and add variable for numnber of items in cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import logoImage from '../assets/logo.jpg';
 
 export default function Header() {
+    const numberOfItemsInCart: number = 0;
+
     return (
         <header className="flex flex-col items-center bg-darkBackground text-white p-4 lg:flex-row lg:justify-between lg:p-6">
             <div className="flex items-center mb-4 lg:mb-0 lg:flex-row lg:items-start">
@@ -8,7 +10,7 @@ export default function Header() {
                 <div className="text-2xl font-bold px-4 py-2">Food Order App</div>
             </div>
             <div className='flex items-center mb-4 lg:mb-0 lg:flex-row lg:items-end'>
-                <a className="text-2xl font-bold px-4 py-2 mr-4 cursor-pointer hover:text-slate-200">Cart</a>
+                <a className="text-2xl font-bold px-4 py-2 mr-4 cursor-pointer hover:text-slate-200">Cart({numberOfItemsInCart})</a>
             </div>
             
         </header>
--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,6 +1,6 @@
 export async function getMealData() {  
   const response = await fetch('http://localhost:3000/meals');
   const mealsJsonData = response.json();
-  console.log("Meals:  ", mealsJsonData);
+  // console.log("Meals:  ", mealsJsonData);
   return mealsJsonData
 }
